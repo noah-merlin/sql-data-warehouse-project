35ee82f060694aa47c9e815b5398ecaca01451c3
Noah <128872134+noah-merlin@users.noreply.github.com>
2025-12-17 14:44:38 -0600

    Update proc_load_bronze.sql

--- scripts/bronze/proc_load_bronze.sql
+++ scripts/bronze/proc_load_bronze.sql
@@ -6,7 +6,7 @@ Script Purpose:
     This stored procedure loads data into the 'bronze' schema from external CSV files. 
     It performs the following actions:
     - Truncates the bronze tables before loading data.
-    - Uses the `BULK INSERT` command to load data from csv Files to bronze tables.
+    - Uses the `BULK INSERT` command to load data from csv files to bronze tables.
 
 Parameters:
     None. 
