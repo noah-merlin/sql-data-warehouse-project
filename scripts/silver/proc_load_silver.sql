e2f712e5e2bc1fd4848e2ec124b08a2d667f2bf4
Noah <128872134+noah-merlin@users.noreply.github.com>
2025-12-18 15:37:28 -0600

    Update proc_load_silver.sql

--- scripts/silver/proc_load_silver.sql
+++ scripts/silver/proc_load_silver.sql
@@ -135,16 +135,28 @@ BEGIN
     		sls_prd_key,
     		sls_cust_id,
     		CASE 
-    			WHEN sls_order_dt = 0 OR LEN(sls_order_dt) != 8 THEN NULL
-    			ELSE CAST(CAST(sls_order_dt AS VARCHAR) AS DATE)
+    			WHEN sls_order_dt = 0
+				OR LEN(sls_order_dt) != 8
+				THEN NULL
+    			ELSE CAST(
+					CAST(sls_order_dt AS VARCHAR)
+					AS DATE)
     		END AS sls_order_dt,
     		CASE 
-    			WHEN sls_ship_dt = 0 OR LEN(sls_ship_dt) != 8 THEN NULL
-    			ELSE CAST(CAST(sls_ship_dt AS VARCHAR) AS DATE)
+    			WHEN sls_ship_dt = 0
+				OR LEN(sls_ship_dt) != 8
+				THEN NULL
+    			ELSE CAST(
+					CAST(sls_ship_dt AS VARCHAR)
+					AS DATE)
     		END AS sls_ship_dt,
     		CASE 
-    			WHEN sls_due_dt = 0 OR LEN(sls_due_dt) != 8 THEN NULL
-    			ELSE CAST(CAST(sls_due_dt AS VARCHAR) AS DATE)
+    			WHEN sls_due_dt = 0
+				OR LEN(sls_due_dt) != 8
+				THEN NULL
+    			ELSE CAST(
+					CAST(sls_due_dt AS VARCHAR)
+					AS DATE)
     		END AS sls_due_dt,
     		CASE 
     			WHEN sls_sales IS NULL OR sls_sales <= 0 OR sls_sales != sls_quantity * ABS(sls_price) 
