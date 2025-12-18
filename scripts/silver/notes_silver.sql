eb68644d52d4e9eeb39f099b23289c232afcaaf3
Noah <128872134+noah-merlin@users.noreply.github.com>
2025-12-18 10:06:49 -0600

    Update notes_silver.sql

--- scripts/silver/notes_silver.sql
+++ scripts/silver/notes_silver.sql
@@ -43,5 +43,53 @@
   SELECT DISTINCT cst_gndr
   FROM bronze.crm_cust_info
 
--- after, quality check silver tables to verify quality of data in the silver layer
+-- crm_prd_info
+
+  -- check for nulls or duplicates in primary key
+  -- expectation: no results
+
+  SELECT
+  prd_id,
+  COUNT(*)
+  FROM bronze.crm_prd_info
+  GROUP BY prd_id
+  HAVING COUNT(*) > 1 OR prd_id IS NULL
+
+  -- check for unwanted spaces in string values
+  -- expectation: no results
+
+  SELECT prd_key
+  FROM bronze.crm_prd_info
+  WHERE prd_key != TRIM(prd_key)
+
+  SELECT prd_nm
+  FROM bronze.crm_prd_info
+  WHERE prd_nm != TRIM(prd_nm)
+  
+  SELECT prd_line
+  FROM bronze.crm_prd_info
+  WHERE prd_line != TRIM(prd_line)
+
+  -- check for nulls or negative numbers
+  -- expectation: no results
+
+  SELECT prd_cost
+  FROM bronze.crm_prd_info
+  WHERE prd_cost < 0 OR prd_cost IS NULL
+
+  -- check the consistency of values in low cardinality columns
+  
+  SELECT DISTINCT prd_line
+  FROM bronze.crm_prd_info
+
+  -- check for invalid date orders 
+  -- (end date must not be earlier than start date)
+
+  SELECT *
+  FROM bronze.crm_prd_info
+  WHERE prd_end_dt < prd_start_dt
+
+-- for complex transformations in SQL, narrow it down to a specific example and brainstorm multiple solution approaches
+
+-- after truncate + insert, quality check silver tables to verify quality of data in the silver layer
 -- replace 'bronze' with 'silver'
