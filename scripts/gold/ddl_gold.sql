5cb72e13508a4c05e24a4a1f3549909235f5dc77
Noah <128872134+noah-merlin@users.noreply.github.com>
2026-01-08 11:12:34 -0600

    Update ddl_gold.sql

--- scripts/gold/ddl_gold.sql
+++ scripts/gold/ddl_gold.sql
@@ -14,9 +14,9 @@ Usage:
 ===============================================================================
 */
 
--- =============================================================================
+-- ===============================================================================
 -- Create Dimension: gold.dim_customers
--- =============================================================================
+-- ===============================================================================
 IF OBJECT_ID('gold.dim_customers', 'V') IS NOT NULL
     DROP VIEW gold.dim_customers;
 GO
@@ -24,18 +24,18 @@ GO
 CREATE VIEW gold.dim_customers AS
 SELECT
     ROW_NUMBER() OVER (ORDER BY cst_id) AS customer_key, -- surrogate key
-    ci.cst_id                          AS customer_id,
-    ci.cst_key                         AS customer_number,
-    ci.cst_firstname                   AS first_name,
-    ci.cst_lastname                    AS last_name,
-    la.cntry                           AS country,
-    ci.cst_marital_status              AS marital_status,
+    ci.cst_id             AS customer_id,
+    ci.cst_key            AS customer_number,
+    ci.cst_firstname      AS first_name,
+    ci.cst_lastname       AS last_name,
+    la.cntry              AS country,
+    ci.cst_marital_status AS marital_status,
     CASE 
         WHEN ci.cst_gndr != 'n/a' THEN ci.cst_gndr -- CRM is the primary source for gender
         ELSE COALESCE(ca.gen, 'n/a')  			   -- fallback to ERP data
-    END                                AS gender,
-    ca.bdate                           AS birthdate,
-    ci.cst_create_date                 AS create_date
+    END                   AS gender,
+    ca.bdate              AS birthdate,
+    ci.cst_create_date    AS create_date
 FROM silver.crm_cust_info ci
 LEFT JOIN silver.erp_cust_az12 ca
     ON ci.cst_key = ca.cid
@@ -43,9 +43,9 @@ LEFT JOIN silver.erp_loc_a101 la
     ON ci.cst_key = la.cid;
 GO
 
--- =============================================================================
+-- ===============================================================================
 -- Create Dimension: gold.dim_products
--- =============================================================================
+-- ===============================================================================
 IF OBJECT_ID('gold.dim_products', 'V') IS NOT NULL
     DROP VIEW gold.dim_products;
 GO
@@ -69,9 +69,9 @@ LEFT JOIN silver.erp_px_cat_g1v2 pc
 WHERE pn.prd_end_dt IS NULL; -- filter out all historical data
 GO
 
--- =============================================================================
+-- ===============================================================================
 -- Create Fact Table: gold.fact_sales
--- =============================================================================
+-- ===============================================================================
 IF OBJECT_ID('gold.fact_sales', 'V') IS NOT NULL
     DROP VIEW gold.fact_sales;
 GO
